398feeb669a5c34c087eab37d817b833b85de481
Antoinelfr <83538367+Antoinelfr@users.noreply.github.com>
2025-10-13 13:20:25 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,8 +14,18 @@ setuptools.setup(
         "Operating System :: WINDOWS/LINUX/MACOS",
     ],
     install_requires=[
-'PySimpleGUI',
-'alive_progress'
-],
+    'PySimpleGUI',
+    'alive_progress',
+    'pillow==11.1.0',
+    'ete3==3.1.3',
+    'six==1.17.0',
+    'PyQt5==5.15.11',
+    'ipykernel==6.29.5',
+    'numpy==1.26.4 ',
+    'pandas==2.2.2',
+    'matplotlib==3.8.4',
+    'scipy==1.11.4',
+    'statsmodels==0.14.1'
+    ],
     python_requires='>=3.6'
 )
