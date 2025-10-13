5e96186a5360d60cd046aa1b65211aad9bcfc0e4
Antoinelfr <83538367+Antoinelfr@users.noreply.github.com>
2025-10-13 13:22:48 +0100

    Update microbELP.py

--- microbELP/microbELP.py
+++ microbELP/microbELP.py
@@ -1,4 +1,11 @@
+import sys, io
+_stdout = sys.stdout
+_stderr = sys.stderr
+sys.stdout = io.StringIO()
+sys.stderr = io.StringIO()
 import PySimpleGUI as sg
+sys.stdout = _stdout
+sys.stderr = _stderr
 import glob
 from microbELP.load_dic import load_dic
 from microbELP.microbiomeAnnotator_condensed import Annotator as ann
